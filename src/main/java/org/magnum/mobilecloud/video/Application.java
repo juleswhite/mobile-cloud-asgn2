59913ebe791746958294b110eb2c41d126b10b1c
Jules White <jules.white@gmail.com>
2021-03-12 09:04:10 -0600

    Removed old comments

--- src/main/java/org/magnum/mobilecloud/video/Application.java
+++ src/main/java/org/magnum/mobilecloud/video/Application.java
@@ -11,23 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 public class Application {
 
 
-	// The app now requires that you pass the location of the keystore and
-	// the password for your private key that you would like to setup HTTPS
-	// with. In Eclipse, you can set these options by going to:
-	//    1. Run->Run Configurations
-	//    2. Under Java Applications, select your run configuration for this app
-	//    3. Open the Arguments tab
-	//    4. In VM Arguments, provide the following information to use the
-	//       default keystore provided with the sample code:
-	//
-	//       -Dkeystore.file=src/main/resources/private/keystore -Dkeystore.pass=changeit
-	//
-	//    5. Note, this keystore is highly insecure! If you want more securtiy, you 
-	//       should obtain a real SSL certificate:
-	//
-	//       http://tomcat.apache.org/tomcat-7.0-doc/ssl-howto.html
-	//
-	// Tell Spring to launch our app!
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
